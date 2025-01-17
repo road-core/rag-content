4ad66561b95b458490d1f4a3cc1312d7ac940ec1
Lucas Alvares Gomes <lucasagomes@gmail.com>
2025-01-17 14:39:44 +0000

    Modify generate_embeddings.py to use common code
    This patch modifies the scripts/generate_embeddings.py to use the common
    code from common_embeddings.py.
    
    Both generate_embeddings.py and generate_embeddings_openstack.py shares
    most of its code now.
    
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>

--- scripts/generate_embeddings.py
+++ scripts/generate_embeddings.py
@@ -1,65 +1,30 @@
 """Utility script to generate embeddings."""
 
-import argparse
-import json
 import os
+import sys
 import time
-from typing import Callable, Dict
+from typing import Dict
 
-import faiss
-import requests
-from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.llms.utils import resolve_llm
 from llama_index.readers.file.flat.base import FlatReader
 
-# from llama_index.core.node_parser import MarkdownNodeParser
-from llama_index.core.schema import TextNode
-from llama_index.core.storage.storage_context import StorageContext
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.vector_stores.faiss import FaissVectorStore
+# Add the common_embedding.py to the Python path
+scripts_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(scripts_dir)
+
+from common_embeddings import (
+    file_metadata_func,
+    get_common_arg_parser,
+    filter_out_invalid_nodes,
+    save_index,
+    save_metadata,
+    process_documents,
+    get_settings,
+    print_unreachable_docs_warning
+)
 
 OCP_DOCS_ROOT_URL = "https://docs.openshift.com/container-platform/"
 OCP_DOCS_VERSION = "4.15"
-UNREACHABLE_DOCS: int = 0
 RUNBOOKS_ROOT_URL = "https://github.com/openshift/runbooks/blob/master/alerts"
-
-
-def ping_url(url: str) -> bool:
-    """Check if the URL parameter is live."""
-    try:
-        response = requests.get(url, timeout=30)
-        return response.status_code == 200
-    except requests.exceptions.RequestException:
-        return False
-
-
-def get_file_title(file_path: str) -> str:
-    """Extract title from the plaintext doc file."""
-    title = ""
-    try:
-        with open(file_path, "r") as file:
-            title = file.readline().rstrip("\n").lstrip("# ")
-    except Exception:  # noqa: S110
-        pass
-    return title
-
-
-def file_metadata_func(file_path: str, docs_url_func: Callable[[str], str]) -> Dict:
-    """Populate the docs_url and title metadata elements with docs URL and the page's title.
-
-    Args:
-        file_path: str: file path in str
-        docs_url_func: Callable[[str], str]: lambda for the docs_url
-    """
-    docs_url = docs_url_func(file_path)
-    title = get_file_title(file_path)
-    msg = f"file_path: {file_path}, title: {title}, docs_url: {docs_url}"
-    if not ping_url(docs_url):
-        global UNREACHABLE_DOCS
-        UNREACHABLE_DOCS += 1
-        msg += ", UNREACHABLE"
-    print(msg)
-    return {"docs_url": docs_url, "title": title}
 
 
 def ocp_file_metadata_func(file_path: str) -> Dict:
@@ -89,47 +54,12 @@ def runbook_file_metadata_func(file_path: str) -> Dict:
     return file_metadata_func(file_path, docs_url)
 
 
-def got_whitespace(text: str) -> bool:
-    """Indicate if the parameter string contains whitespace."""
-    for c in text:
-        if c.isspace():
-            return True
-    return False
-
-
 if __name__ == "__main__":
 
     start_time = time.time()
 
-    parser = argparse.ArgumentParser(description="embedding cli for task execution")
-    parser.add_argument("-f", "--folder", help="Plain text folder path")
+    parser = get_common_arg_parser()
     parser.add_argument("-r", "--runbooks", help="Runbooks folder path")
-    parser.add_argument(
-        "-md",
-        "--model-dir",
-        default="embeddings_model",
-        help="Directory containing the embedding model",
-    )
-    parser.add_argument(
-        "-mn",
-        "--model-name",
-        help="HF repo id of the embedding model",
-    )
-    parser.add_argument(
-        "-c", "--chunk", type=int, default=380, help="Chunk size for embedding"
-    )
-    parser.add_argument(
-        "-l", "--overlap", type=int, default=0, help="Chunk overlap for embedding"
-    )
-    parser.add_argument(
-        "-em",
-        "--exclude-metadata",
-        nargs="+",
-        default=None,
-        help="Metadata to be excluded during embedding",
-    )
-    parser.add_argument("-o", "--output", help="Vector DB output folder")
-    parser.add_argument("-i", "--index", help="Product index")
     parser.add_argument("-v", "--ocp-version", help="OCP version")
     args = parser.parse_args()
     print(f"Arguments used: {args}")
@@ -150,75 +80,36 @@ if __name__ == "__main__":
 
     os.environ["HF_HOME"] = args.model_dir
     os.environ["TRANSFORMERS_OFFLINE"] = "1"
-    Settings.chunk_size = args.chunk
-    Settings.chunk_overlap = args.overlap
-    Settings.embed_model = HuggingFaceEmbedding(model_name=args.model_dir)
-    Settings.llm = resolve_llm(None)
 
-    embedding_dimension = len(Settings.embed_model.get_text_embedding("random text"))
-    faiss_index = faiss.IndexFlatIP(embedding_dimension)
-    vector_store = FaissVectorStore(faiss_index=faiss_index)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    settings, embedding_dimension, storage_context = get_settings(
+        args.chunk, args.overlap, args.model_dir)
 
     # Load documents
-    documents = SimpleDirectoryReader(
-        args.folder, recursive=True, file_metadata=ocp_file_metadata_func
-    ).load_data()
-
-    # Split based on header/section
-    # md_parser = MarkdownNodeParser()
-    # documents = md_parser.get_nodes_from_documents(documents)
+    documents = process_documents(
+        args.folder, metadata_func=ocp_file_metadata_func)
 
     # Create chunks/nodes
-    nodes = Settings.text_splitter.get_nodes_from_documents(documents)
+    nodes = settings.text_splitter.get_nodes_from_documents(documents)
 
     # Filter out invalid nodes
-    good_nodes = []
-    for node in nodes:
-        if isinstance(node, TextNode) and got_whitespace(node.text):
-            # Exclude given metadata during embedding
-            # if args.exclude_metadata is not None:
-            #     node.excluded_embed_metadata_keys.extend(args.exclude_metadata)
-            good_nodes.append(node)
-        else:
-            print("skipping node without whitespace: " + node.__repr__())
+    good_nodes = filter_out_invalid_nodes(nodes)
 
-    runbook_documents = SimpleDirectoryReader(
-        args.runbooks,
-        recursive=True,
-        required_exts=[".md"],
-        file_extractor={".md": FlatReader()},
-        file_metadata=runbook_file_metadata_func,
-    ).load_data()
-    runbook_nodes = Settings.text_splitter.get_nodes_from_documents(runbook_documents)
+    # Load runbook documents
+    runbook_documents = process_documents(
+        args.runbooks, metadata_func=runbook_file_metadata_func,
+        required_exts=['.md',], file_extractor={".md": FlatReader()})
 
+    # Create chunks/nodes
+    runbook_nodes = settings.text_splitter.get_nodes_from_documents(documents)
+
+    # Extend nodes with runbook_nodes
     good_nodes.extend(runbook_nodes)
 
     # Create & save Index
-    index = VectorStoreIndex(
-        good_nodes,
-        storage_context=storage_context,
-    )
-    index.set_index_id(args.index)
-    index.storage_context.persist(persist_dir=PERSIST_FOLDER)
+    save_index(good_nodes, storage_context, args.index, PERSIST_FOLDER)
 
-    metadata: dict = {}
-    metadata["execution-time"] = time.time() - start_time
-    metadata["llm"] = "None"
-    metadata["embedding-model"] = args.model_name
-    metadata["index-id"] = args.index
-    metadata["vector-db"] = "faiss.IndexFlatIP"
-    metadata["embedding-dimension"] = embedding_dimension
-    metadata["chunk"] = args.chunk
-    metadata["overlap"] = args.overlap
-    metadata["total-embedded-files"] = len(documents)
+    # Save metadata
+    save_metadata(start_time, args, embedding_dimension, documents,
+                  PERSIST_FOLDER)
 
-    with open(os.path.join(PERSIST_FOLDER, "metadata.json"), "w") as file:
-        file.write(json.dumps(metadata))
-
-    if UNREACHABLE_DOCS > 0:
-        print("WARNING:\n"
-            f"There were documents with {UNREACHABLE_DOCS} unreachable URLs, "
-            "grep the log for UNREACHABLE.\n"
-            "Please update the plain text."
-        )
+    print_unreachable_docs_warning()
